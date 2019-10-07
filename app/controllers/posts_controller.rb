6d6145979c720ef6c4cccb27d96fbaf5f553dd69
wat <monocle888@gmail.com>
2019-10-07 15:37:46 +0000

    developed new create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,23 @@
 class PostsController < ApplicationController
   def index
+    @post=Post.all
   end
 
   def new
+    @post=Post.new
+  end
+  
+  def create
+    @post=Post.new(post_params)
+    @post.save
+    redirect_to posts_path
   end
 
   def edit
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title,:url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/new'
-
-  get 'posts/edit'
+  root "posts#index"
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
