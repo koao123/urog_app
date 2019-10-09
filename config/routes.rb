979bdb930261410b14af3b8ddb028ffc1a94656b
wat <monocle888@gmail.com>
2019-10-09 13:44:25 +0000

    modeling-users-model-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "posts#index"
+  root 'static_pages#home'
+  resources :users
   resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191007122410) do
+ActiveRecord::Schema.define(version: 20191008141703) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", limit: 16, null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
