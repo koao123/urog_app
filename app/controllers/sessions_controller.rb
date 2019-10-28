dab4c52056cdedc939fd3352d496efcc1e647c57
wat <monocle888@gmail.com>
2019-10-28 09:47:59 +0000

    change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
      log_in user
      redirect_to root_url
     else
+     flash.now[:danger] = 'Invalid email/password combination'
      render 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,14 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
     has_many :posts, dependent: :destroy
+    
     def feed
         Post.where("user_id=?",id)
     end
+    
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
