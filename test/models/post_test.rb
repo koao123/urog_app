f72364a43ad9c45f1df642505c70611d209b5238
wat <monocle888@gmail.com>
2019-11-03 14:21:52 +0000

    half-completed

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user=users(:michael)
+    @post=@user.posts.new(title: "aaa",url: "aaaaa")
+  end
+  test "should be valid" do
+    assert @post.valid?
+  end
+  test "user_id should be present" do
+    @post.user_id=nil
+    assert_not @post.valid?
+  end
+  test "url should be present" do
+    @post.url="   "
+    assert_not @post.valid?
+  end
+  test "order should be most recent first" do
+    assert_equal posts(:most_recent), Post.first
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,5 +49,11 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
+  test "associated posts should be destroyed" do
+    @user.save
+    @user.posts.create!(title: "aabbb",url:"ccddd")
+    assert_difference 'Post.count', -1 do
+     @user.destroy
+    end
+  end
 end
